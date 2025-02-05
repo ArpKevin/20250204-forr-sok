4c48500ac168cce20588fb742d9b4488bc79716c
ArpKevin <arpaskevin11@gmail.com>
2025-02-05 11:23:37 +0100

    began the backend segment

---
+++ backend/read.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('connect.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "GET"){   
+    if(isset($_GET['id']) && is_numeric($_GET['id'])){
+        $termekek = mysqli_query($conn, "select * from products where id = " . $_GET['id']);
+        if(!$termekek){
+            http_response_code(404);
+            echo json_encode([
+                "status" => "error",
+                "message" => "A megadott azonosítóval nem található termék: " . $_GET['id']
+            ]);
+        }
+    }
+    else{
+        echo "Helytelen paraméter.";
+    }
+}
+else{
+    http_response_code(405);
+}
+
+?>
