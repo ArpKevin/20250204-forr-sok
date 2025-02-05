c11b30019fb02b9c90c0c55d65cf54bc21c60087
ArpKevin <arpaskevin11@gmail.com>
2025-02-05 14:50:49 +0100

    continued the backend segment

--- backend/api.php
+++ backend/api.php
@@ -2,27 +2,43 @@
 
 require_once('connect.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "GET"){   
-    if(isset($_GET['id']) && is_numeric($_GET['id'])){
-        $termekek = mysqli_query($conn, "select * from products where id = " . $_GET['id']);
-        if(!$termekek){
-            http_response_code(404);
-            echo json_encode([
-                "status" => "error",
-                "message" => "A megadott azonosítóval nem található termék: " . $_GET['id']
-            ]);
-        }
-        else{
-            echo json_encode($termekek);
-        }
-        
+header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
+$torzs = json_decode(
+    '
+    {
+        "name": "New Product",
+        "description": "Product description",
+        "price": 99.99,
+        "image_name": "new_product.jpg"
+      }
+    ', true
+    );
+    echo json_encode($torzs);
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+        $termekek = mysqli_query($conn, "SELECT * FROM products WHERE id = " . $_GET['id']);
+    } else {
+        $termekek = mysqli_query($conn, "SELECT * FROM products");
     }
-    else{
-        echo "Helytelen paraméter.";
+
+    if (mysqli_num_rows($termekek) === 0) {
+        http_response_code(404);
+        echo json_encode([
+            "status" => "error",
+            "message" => "A megadott azonosítóval nem található termék: " . $_GET['id']
+        ]);
+    } else {
+        $result = mysqli_fetch_all($termekek, MYSQLI_ASSOC);
+        echo json_encode($result, JSON_PRETTY_PRINT);
     }
 }
-else{
-    http_response_code(405);
+else if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    
 }
 
 ?>
