6672a22d39f1acd73b63903067344dfea970db47
ArpKevin <arpaskevin11@gmail.com>
2025-02-11 12:00:10 +0100

    backend segment finished

--- backend/api.php
+++ backend/api.php
@@ -113,4 +113,43 @@ else if ($_SERVER["REQUEST_METHOD"] == "PUT"){
         ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     }
 }
+else if ($_SERVER["REQUEST_METHOD"] == "DELETE"){
+    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+        $termekek = mysqli_query($conn, "DELETE FROM products WHERE id = " . $_GET['id']);
+
+        if ($termekek){
+            http_response_code(204);
+        }
+        else{
+            http_response_code(400);
+            echo json_encode([
+                "status" => "error",
+                "message" => "Hiba a termék törlése során: " . mysqli_error($conn)
+            ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        }
+
+        if (mysqli_num_rows($termekek) === 0) {
+            http_response_code(404);
+            echo json_encode([
+                "status" => "error",
+                "message" => "A megadott azonosítóval nem található termék: " . $_GET['id']
+            ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        } else {
+            $result = mysqli_fetch_all($termekek, MYSQLI_ASSOC);
+            // echo json_encode($result, JSON_PRETTY_PRINT);
+            echo json_encode([
+                "status" => "success",
+                "data" => $result
+            ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        }
+    } 
+    else{
+        http_response_code(400);
+        echo json_encode([
+            "status" => "error",
+            "message" => "Hiányzik a termék azonosító (id), vagy nem megfelelő formátumú."
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+    
+}
 ?>
