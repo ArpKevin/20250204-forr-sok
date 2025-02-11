218ad3acfb2a5bba3f3a82d2bb8e66633703255f
ArpKevin <arpaskevin11@gmail.com>
2025-02-11 10:35:39 +0100

    post method done

--- backend/api.php
+++ backend/api.php
@@ -7,8 +7,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 
 
-
-
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (isset($_GET['id']) && is_numeric($_GET['id'])) {
         $termekek = mysqli_query($conn, "SELECT * FROM products WHERE id = " . $_GET['id']);
@@ -35,31 +33,29 @@ else if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $data = json_decode(file_get_contents("php://input"), true);
 
-    $name = $data['name'] ?? null;
-    $description = $data['description'] ?? null;
-    $price = $data['price'] ?? null;
-    $image_name = $data['image_name'] ?? null;
+    if (isset($data['name'], $data['description'], $data['price'], $data['image_name'])
+        && trim($data['name']) !== "" && trim($data['description']) !== "" && is_numeric($data['price']) && trim($data['image_name']) !== ""
+    ){
 
-    if ($name && $description && $price && $image_name){
+        $name = mysqli_real_escape_string($conn, $data['name']);
+        $description = mysqli_real_escape_string($conn, $data['description']);
+        $price = doubleval($data['price']);
+        $image_name = mysqli_real_escape_string($conn, $data['image_name']);
 
-        $stmt = $conn->prepare("INSERT INTO products (name, description, price, image_name) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("ssss", $name, $description, $price, $image_name);
 
-        echo json_encode([
-            "status" => "success",
-            "message" => "asdsads"
-        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        $query = mysqli_query($conn,"INSERT INTO products (name, description, price, image_name) VALUES ('$name', '$description', $price, '$image_name')");
 
-        if ($stmt->execute()) {
+        if ($query){
             echo json_encode([
                 "status" => "success",
-                "message" => "Product added successfully"
+                "message" => "Termék sikeresen létrehozva.",
+                "id" => mysqli_insert_id($conn)
             ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        } else {
-            http_response_code(400);
+        }
+        else{
             echo json_encode([
                 "status" => "error",
-                "message" => "Failed to add product."
+                "message" => "Hiba a termék létrehozása során: " . mysqli_error($conn)
             ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         }
     }
